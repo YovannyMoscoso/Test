f2a4af308b4ad04ffc26c7cc9dfc7c1e4508ab12
Yovanny Moscoso <111789843+YovannyMoscoso@users.noreply.github.com>
2023-01-23 10:00:37 -0500

    Method to sum integer numbers

--- src/domain/InterviewQuestion1.java
+++ src/domain/InterviewQuestion1.java
@@ -1,4 +1,13 @@
+ /* This is going to be an easy example to taste my gits */
 package domain;
 
 public class InterviewQuestion1 {
+  // Printing the sum of numbers in an array
+  public static int sum(arr[] numbers){
+    int sum =0;
+    for(int i =0; i<arr.length; i++){
+        sum += arr[i];
+    }
+    return sum;
+  }
 }
