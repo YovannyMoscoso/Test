4f4b537a8d7f65b9173e70d901752e74bb87404b
Yovanny Moscoso <yovanny.moscoso14@gmail.com>
2023-01-23 10:28:50 -0500

    modifying my method

--- src/domain/InterviewQuestion1.java
+++ src/domain/InterviewQuestion1.java
@@ -3,7 +3,7 @@ package domain;
 
 public class InterviewQuestion1 {
   // Printing the sum of numbers in an array
-  public static int sum(arr[] numbers){
+  public static int sum(int[] arr){
     int sum =0;
     for(int i =0; i<arr.length; i++){
         sum += arr[i];
