fce7315009c07d46b9698112b1e6fd7aa4dfc611
Yovanny Moscoso <yovanny.moscoso14@gmail.com>
2023-01-23 10:42:01 -0500

    Updating Main to user method sum

--- src/test/Main.java
+++ src/test/Main.java
@@ -1,7 +1,11 @@
 package test;
+import static domain.InterviewQuestion1.sum;
 
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hello world!");
+        int[] myArray = {1,2,3,4,5};
+        int result = sum(myArray);
+        System.out.println("The result of adding the numbers is: " + result);
     }
 }
